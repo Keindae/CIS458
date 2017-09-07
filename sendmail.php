c8145c0f3b5a75360772ea7c046e094c5a2e8ff2
Keindae <noblettm@mail.gvsu.edu>
2017-09-07 12:56:23 -0400

    458 Lab 1

---
+++ sendmail.php
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h1>Send Phising Emails</h1>
+<?php
+mail('noblettm@mail.gvsu.edu', 'Your account is hacked', 'Please call us',
+    'From: turmp@whitehouse.gov');
+?>
+
+</body>
+
+</html>
