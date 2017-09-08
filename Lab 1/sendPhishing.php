f72cfbf7388bf285337b6bac7551a4a533629e76
Keindae <noblettm@mail.gvsu.edu>
2017-09-07 20:41:13 -0400

    Added list to webpage

--- Lab 1/sendPhishing.php
+++ Lab 1/sendPhishing.php
@@ -7,7 +7,14 @@ $headers = "From: " .$from;
 
 mail($to,$subject,$content,$headers);
 
-echo "<h1>Eamil was sent out <br><br>";
+echo "<h1>Creating a List</h1>";
+echo "<ul>
+        <li>Songs</li>
+        <li>Dance</li>
+        <li>Workout</li>
+        </ul>";
+
+echo "<h1>Email was sent out <br><br>";
 echo "<h1>Please do not misuse this site. You will be caught<br></h1>";
 
 ?>
